6d5f7dbd2b1de6e0ffc5e6a1476a3ca537995d97
Andrew <jayedeem@gmail.com>
2020-10-17 12:17:56 -0700

    wk1 assignment completed + refactor

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,32 @@ import { Card, ListItem } from 'react-native-elements';
 
 import { PARTNERS } from '../shared/partners';
 
+const renderPartner = ({ item }) => {
+  return (
+    <ListItem
+      title={item.name}
+      subtitle={item.description}
+      leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
+    />
+  );
+};
+
+const MissionStatement = ({ title }) => {
+  return (
+    <Card title={title} Divider>
+      <Text>
+        We present a curated database of the best campsites in the vast woods
+        and backcountry of the World Wide Web Wilderness. We increase access to
+        adventure for the public while promoting safe and respectful use of
+        resources. The expert wilderness trekkers on our staff personally verify
+        each campsite to make sure that they are up to our standards. We also
+        present a platform for campers to share reviews on campsites they have
+        visited with each other.
+      </Text>
+    </Card>
+  );
+};
+
 class About extends Component {
   constructor(props) {
     super(props);
@@ -15,29 +41,9 @@ class About extends Component {
     title: 'About Us',
   };
   render() {
-    const renderPartner = ({ item }) => {
-      return (
-        <ListItem
-          title={item.name}
-          subtitle={item.description}
-          leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
-        />
-      );
-    };
-
     return (
       <ScrollView>
-        <Card title="Our Mission" Divider>
-          <Text>
-            We present a curated database of the best campsites in the vast
-            woods and backcountry of the World Wide Web Wilderness. We increase
-            access to adventure for the public while promoting safe and
-            respectful use of resources. The expert wilderness trekkers on our
-            staff personally verify each campsite to make sure that they are up
-            to our standards. We also present a platform for campers to share
-            reviews on campsites they have visited with each other.
-          </Text>
-        </Card>
+        <MissionStatement title="Our Mission" />
         <Card title="Community Partners" Divider>
           <FlatList
             data={this.state.partners}
