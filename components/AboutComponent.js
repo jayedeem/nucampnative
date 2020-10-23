4f2469eb9ffd8cb232242a6e2c915254d44d46eb
Andrew <jayedeem@gmail.com>
2020-10-22 18:45:57 -0700

    loading component

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -3,22 +3,12 @@ import { ScrollView, Text, FlatList } from 'react-native';
 import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
+import Loading from './LoadingComponent';
 
 const mapStateToProps = (state) => {
   return {
     partners: state.partners,
   };
-};
-
-const renderPartner = ({ item }) => {
-  console.log('renderPartner', item);
-  return (
-    <ListItem
-      title={item.name}
-      subtitle={item.description}
-      leftAvatar={{ source: { uri: baseUrl + item.image } }}
-    />
-  );
 };
 
 const MissionStatement = ({ title }) => {
@@ -41,7 +31,38 @@ class About extends Component {
   static navigationOptions = {
     title: 'About Us',
   };
+
   render() {
+    const renderPartner = ({ item }) => {
+      console.log('renderPartner', item);
+      return (
+        <ListItem
+          title={item.name}
+          subtitle={item.description}
+          leftAvatar={{ source: { uri: baseUrl + item.image } }}
+        />
+      );
+    };
+    if (this.props.partners.isLoading) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Loading />
+          </Card>
+        </ScrollView>
+      );
+    }
+    if (this.props.partners.errMess) {
+      return (
+        <ScrollView>
+          <Mission />
+          <Card title="Community Partners">
+            <Text>{this.props.partners.errMess}</Text>
+          </Card>
+        </ScrollView>
+      );
+    }
     return (
       <ScrollView>
         <MissionStatement title="Our Mission" />
